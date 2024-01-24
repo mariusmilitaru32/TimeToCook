4422a36c14cc60f11d9da346c45d436f64221d40
mariusmilitaru32 <marius.militaru20@gmail.com>
2024-01-24 21:13:34 +0000

    Create __init__.py for flask app

---
+++ timetocook/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__, static_folder="static", static_url_path="")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+db = SQLAlchemy(app)
+
+from timetocook import routes
