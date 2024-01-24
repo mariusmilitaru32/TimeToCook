dd4cbd2f892c1ebd30cb69168c01e32b53e0576e
mariusmilitaru32 <marius.militaru20@gmail.com>
2024-01-24 22:36:26 +0000

    Add user db model

--- timetocook/templates/models.py
+++ timetocook/templates/models.py
@@ -1,1 +1,18 @@
 from timetocook import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    admin = db.Column(db.Boolean, default=False)
+    banned = db.Column(db.Boolean, default=False)
+    recipes = db.relationship(
+        "Recipe", backref="user", cascade="all, delete", lazy=True
+    )
+    favorite_recipes = db.relationship("UserFavorite", backref="user", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<User username={self.username}>"
